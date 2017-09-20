b0094b15c6737c237a9f49559ef00eb92cf32fa3
poojapawar123 <31065860+poojapawar123@users.noreply.github.com>
2017-09-20 11:15:49 -0700

    first commit on 20/9/2017

---
+++ src/main/java/demo/GradleDemo.java
@@ -0,0 +1,15 @@
+package demo;
+
+
+class GradleDemo{
+
+           public static void main(String args[])
+          {
+                for(int i=0;i<=5;i++)
+               {
+                    System.out.println("Hello!!!..Welcome to Gradle world"+i);
+                }
+           }
+
+
+}
